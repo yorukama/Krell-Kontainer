2dd91336a16720fc0fb619096c479b3a0dd2b194
yorukama <jacobakrell@gmail.com>
2020-10-30 17:56:05 -0500

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -7,7 +7,7 @@ module.exports = async function(deployer) {
   const tokenMock = await DaiTokenMock.deployed()
   // Mint 1,000 Dai Tokens for the deployer
   await tokenMock.mint(
-    '0x23711fF3eD42a36b2b70276Adc4deA19f72d2AEC',
+    '0xD1F0444bd81833eBC7BF4dCBe99eA87B468a476C',
     '1000000000000000000000'
   )
 };
